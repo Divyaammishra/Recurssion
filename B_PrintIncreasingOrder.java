860216dec08f049ae8c7240c243bfb96d14adc2f
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2023-08-11 23:16:25 +0530

    Add files via upload

---
+++ B_PrintIncreasingOrder.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class B_Increasing {
+
+    public static void printInc(int number){
+
+        //Base case
+        if(number == 1){
+            System.out.print(number+" ");
+            return;
+        }
+
+        //Calling Inner Function
+        printInc(number-1);
+
+        //Defining, to do
+        System.out.print(number+" ");
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        //Calling function
+        printInc(number);
+
+    }
+}
